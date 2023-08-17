05708a153ce505284b9822e40f44caa756b09533
Taras <taras.sydor@outlook.com>
2023-08-18 01:25:21 +0300

    update Subscription tab

--- src/pages/course/SubscriptionTab.jsx
+++ src/pages/course/SubscriptionTab.jsx
@@ -17,64 +17,87 @@ const TypographyStyled = styled(Typography)({
 
 const SubscriptionTab = () => {
   return (
-    <>
+    <Box sx={{ borderBottom: "1px solid rgba(191,190,187,0.5)", pb: "60px" }}>
       <Stack
         flexDirection="row"
         alignItems="center"
         justifyContent="space-between"
+        gap="15px"
         mb="17px"
       >
         <Typography variant="sectionTitle" component="h2" fontSize="35px">
           Subscription
         </Typography>
-        <Button variant="yellow">Renew Your Subscription</Button>
+        <Button variant="yellow" sx={{ height: "54px" }}>
+          Renew Your Subscription
+        </Button>
       </Stack>
-      <BlockBordered>
-        <Stack
-          flexDirection="row"
-          alignItems="flex-start"
-          justifyContent="space-between"
-          gap="25px"
-        >
-          <Box>
-            <TypographyStyled>
-              <span>Subcription date:</span> 25. June 2023
-            </TypographyStyled>
-            <TypographyStyled>
-              <span>Expiration date:</span> 25. July 2023
-            </TypographyStyled>
-            <TypographyStyled>
-              <span>Progress:</span> Advanced
-            </TypographyStyled>
-            <Typography
-              pl="172px"
-              fontSize="14px"
-              fontWeight={600}
-              color="#BFBEBB"
-            >
-              (25 hours of watching & listening, 17 comments, 3 questions)
-            </Typography>
-          </Box>
-          <Box
-            maxWidth="264px"
-            alignSelf="flex-end"
-            flexShrink={0}
-            textAlign="right"
+      <Stack
+        flexDirection="row"
+        alignItems="flex-start"
+        justifyContent="space-between"
+        gap="25px"
+      >
+        <BlockBordered>
+          <TypographyStyled>
+            <span>Subcription date:</span> 25. June 2023
+          </TypographyStyled>
+          <TypographyStyled>
+            <span>Expiration date:</span> 25. July 2023
+          </TypographyStyled>
+          <TypographyStyled>
+            <span>Progress:</span> 25% Completed
+          </TypographyStyled>
+        </BlockBordered>
+        <Box maxWidth="314px" flexShrink={0} textAlign="right" mt="50px">
+          <Typography
+            component="p"
+            fontSize="16px"
+            fontWeight={600}
+            lineHeight="24px"
+            mb="15px"
           >
+            Buy Lifetime Subscription without any expirations today with
+            discounted price.
+          </Typography>
+          <Typography component="p" color="#BFBEBB" fontWeight={600}>
+            Discounted price:{" "}
             <Typography
-              fontSize="14px"
-              fontWeight={600}
-              lineHeight="22px"
-              mb="20px"
+              component="span"
+              fontSize="25px"
+              color="#026670"
+              fontWeight={700}
+              ml="8px"
             >
-              You can also buy Lifetime Subscription for $499,59 without any
-              expirations.
+              $599.49
             </Typography>
-            <TextLink to="">Buy Lifetime Subcription</TextLink>
-          </Box>
-        </Stack>
-      </BlockBordered>
-    </>
+          </Typography>
+        </Box>
+      </Stack>
+      <Stack
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+        mt="20px"
+        gap="15px"
+      >
+        <Button variant="yellow" sx={{ height: "54px" }}>
+          Add Associate for $25.49
+        </Button>
+        <Button
+          variant="outlined"
+          sx={{
+            height: "54px",
+            borderWidth: "2px",
+            "&:hover": {
+              border: "2px solid #026670",
+            },
+          }}
+        >
+          Buy Lifetime Subsctiption
+        </Button>
+      </Stack>
+    </Box>
   );
 };
 
