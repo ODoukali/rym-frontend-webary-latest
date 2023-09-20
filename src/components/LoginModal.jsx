cac4411fa42b9a2680b1d6d7f78a2e251155c35f
Taras <taras.sydor@outlook.com>
2023-09-20 03:05:55 +0300

    added slide effect on login forms

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -39,9 +39,9 @@ const ToggleButtonStyled = styled(ToggleButton)({
   },
   "&.Mui-selected": {
     color: "#026670",
-    backgroundColor: "#EDECE8",
+    backgroundColor: "transparent",
     "&:hover": {
-      backgroundColor: "#EDECE8",
+      backgroundColor: "transparent",
     },
   },
 });
@@ -85,6 +85,28 @@ const TextFieldStyled = styled(TextField)({
   "& input": {
     borderRadius: "30px !important",
     padding: "15px 40px",
+  },
+});
+
+const ToggleButtonGroupStyled = styled(ToggleButtonGroup)({
+  position: "relative",
+  width: "300px",
+  justifyContent: "space-between",
+  backgroundColor: "#BFBEBB",
+  borderRadius: "30px",
+  padding: "2px",
+  "&::before": {
+    content: "''",
+    width: "147px",
+    height: "50px",
+    position: "absolute",
+    left: 2,
+    backgroundColor: "#EDECE8",
+    borderRadius: "30px",
+    transition: "left 0.2s ease-in-out",
+  },
+  "&.active::before": {
+    left: 151,
   },
 });
 
@@ -161,165 +183,168 @@ const LoginModal = (props) => {
       </Stack>
       <Box bgcolor="#F7F6F2" p="0px 50px 40px">
         <Box position="relative" textAlign="center" mt="-27px" mb="40px">
-          <ToggleButtonGroup
+          <ToggleButtonGroupStyled
+            className={`${register === "signup" ? "active" : ""}`}
             value={register}
             exclusive
             onChange={handleChange}
-            sx={{
-              width: "300px",
-              justifyContent: "space-between",
-              backgroundColor: "#BFBEBB",
-              borderRadius: "30px",
-              p: "2px",
-            }}
           >
             <ToggleButtonStyled value="login">Log In</ToggleButtonStyled>
             <ToggleButtonStyled value="signup">Sign Up</ToggleButtonStyled>
-          </ToggleButtonGroup>
+          </ToggleButtonGroupStyled>
         </Box>
-
-        {register === "login" ? (
-          <Box textAlign="center">
-            <ButtonGoogle fullWidth variant="outlined" startIcon={<Google />}>
-              Continue with Google
-            </ButtonGoogle>
-            <Hr>
-              <Typography fontSize="14px" fontWeight={600} color="#BFBEBB">
-                or
-              </Typography>
-            </Hr>
-            <Stack gap="10px">
-              <TextFieldStyled
-                name="loginEmail"
-                type="email"
-                placeholder="Email"
-                value={loginData.loginEmail}
-                onChange={(e) => inputChange("loginEmail", e.target.value)}
-              />
-              <TextFieldStyled
-                name="loginPassword"
-                type="password"
-                placeholder="Password"
-                value={loginData.loginPassword}
-                onChange={(e) => inputChange("loginPassword", e.target.value)}
-              />
-            </Stack>
-            <ButtonLogin variant="yellow">Log In</ButtonLogin>
-            <Switcher
-              name="keepLogged"
-              label="Keep me logged in?"
-              defaultChecked
-            />
-            <Divider sx={{ borderColor: "#BFBEBB", my: "40px" }} />
-            <Link
-              href=""
-              underline="hover"
-              sx={{ fontSize: "12px", fontWeight: 600, color: "#BFBEBB" }}
-            >
-              Forgot Password?
-            </Link>
-          </Box>
-        ) : null}
-        {register === "signup" ? (
-          <Box textAlign="center">
-            <ButtonGoogle fullWidth variant="outlined" startIcon={<Google />}>
-              Continue with Google
-            </ButtonGoogle>
-            <Hr>
-              <Typography fontSize="14px" fontWeight={600} color="#BFBEBB">
-                or
-              </Typography>
-            </Hr>
-            <Stack gap="10px">
-              <TextFieldStyled name="name" type="text" placeholder="Name" />
-              <TextFieldStyled
-                name="nickname"
-                type="text"
-                placeholder="Nickname (optional)"
-                value={loginData.nickname}
-                onChange={(e) => inputChange("nickname", e.target.value)}
-              />
-              <TextFieldStyled
-                name="phone"
-                type="text"
-                placeholder="Phone"
-                InputProps={{
-                  inputComponent: PhoneMask,
-                }}
-                value={loginData.phone}
-                onChange={(e) => inputChange("phone", e.target.value)}
-              />
-              <TextFieldStyled
-                name="signupEmail"
-                type="email"
-                placeholder="Email"
-                value={loginData.signupEmail}
-                onChange={(e) => inputChange("signupEmail", e.target.value)}
-              />
-              <TextFieldStyled
-                name="signupPassword"
-                type="password"
-                placeholder="Password"
-                value={loginData.signupPassword}
-                onChange={(e) => inputChange("signupPassword", e.target.value)}
-              />
-              <TextFieldStyled
-                name="signupRePassword"
-                type="password"
-                placeholder="Retype Password"
-                value={loginData.signupRePassword}
-                onChange={(e) =>
-                  inputChange("signupRePassword", e.target.value)
-                }
-              />
-            </Stack>
-            <Stack
-              flexDirection="row"
-              alignItems="center"
-              justifyContent="center"
-              gap="15px"
-              mt="37px"
-            >
-              <FormControlLabel
-                control={<Checkbox defaultChecked />}
-                label="I am not a robot!"
-              />
-              {/* <ReCAPTCHA sitekey="Your client site key" /> */}
-            </Stack>
-            <ButtonLogin variant="yellow">Create Account</ButtonLogin>
-            <Stack alignItems="center" gap="10px">
+        <Box position="relative">
+          <Stack
+            className={`${register === "signup" ? "active" : ""}`}
+            width="200%"
+            flexDirection="row"
+            sx={{
+              transition: "transform 0.2s ease-in-out",
+              "&.active": { transform: "translateX(-50%)" },
+            }}
+          >
+            <Box width="100%" textAlign="center">
+              <ButtonGoogle fullWidth variant="outlined" startIcon={<Google />}>
+                Continue with Google
+              </ButtonGoogle>
+              <Hr>
+                <Typography fontSize="14px" fontWeight={600} color="#BFBEBB">
+                  or
+                </Typography>
+              </Hr>
+              <Stack gap="10px">
+                <TextFieldStyled
+                  name="loginEmail"
+                  type="email"
+                  placeholder="Email"
+                  value={loginData.loginEmail}
+                  onChange={(e) => inputChange("loginEmail", e.target.value)}
+                />
+                <TextFieldStyled
+                  name="loginPassword"
+                  type="password"
+                  placeholder="Password"
+                  value={loginData.loginPassword}
+                  onChange={(e) => inputChange("loginPassword", e.target.value)}
+                />
+              </Stack>
+              <ButtonLogin variant="yellow">Log In</ButtonLogin>
               <Switcher
-                name="keepSignedin"
-                label="Keep me signed in."
+                name="keepLogged"
+                label="Keep me logged in?"
                 defaultChecked
               />
-              <Switcher
-                name="sendEmails"
-                label="Send me emails about updates and news."
-                defaultChecked
-              />
-            </Stack>
-            <Divider sx={{ borderColor: "#BFBEBB", my: "40px" }} />
-            <Typography
-              maxWidth="300px"
-              fontSize="12px"
-              fontWeight={500}
-              lineHeight="16px"
-              color="#BFBEBB"
-              margin="auto"
-            >
-              By creating an account, you are agreeing to our{" "}
-              <Link href="" underline="hover" color="#026670">
-                Terms of Service
-              </Link>{" "}
-              and{" "}
-              <Link href="" underline="hover" color="#026670">
-                Privacy Policy
+              <Divider sx={{ borderColor: "#BFBEBB", my: "40px" }} />
+              <Link
+                href=""
+                underline="hover"
+                sx={{ fontSize: "12px", fontWeight: 600, color: "#BFBEBB" }}
+              >
+                Forgot Password?
               </Link>
-              .
-            </Typography>
-          </Box>
-        ) : null}
+            </Box>
+            <Box width="100%" textAlign="center">
+              <ButtonGoogle fullWidth variant="outlined" startIcon={<Google />}>
+                Continue with Google
+              </ButtonGoogle>
+              <Hr>
+                <Typography fontSize="14px" fontWeight={600} color="#BFBEBB">
+                  or
+                </Typography>
+              </Hr>
+              <Stack gap="10px">
+                <TextFieldStyled name="name" type="text" placeholder="Name" />
+                <TextFieldStyled
+                  name="nickname"
+                  type="text"
+                  placeholder="Nickname (optional)"
+                  value={loginData.nickname}
+                  onChange={(e) => inputChange("nickname", e.target.value)}
+                />
+                <TextFieldStyled
+                  name="phone"
+                  type="text"
+                  placeholder="Phone"
+                  InputProps={{
+                    inputComponent: PhoneMask,
+                  }}
+                  value={loginData.phone}
+                  onChange={(e) => inputChange("phone", e.target.value)}
+                />
+                <TextFieldStyled
+                  name="signupEmail"
+                  type="email"
+                  placeholder="Email"
+                  value={loginData.signupEmail}
+                  onChange={(e) => inputChange("signupEmail", e.target.value)}
+                />
+                <TextFieldStyled
+                  name="signupPassword"
+                  type="password"
+                  placeholder="Password"
+                  value={loginData.signupPassword}
+                  onChange={(e) =>
+                    inputChange("signupPassword", e.target.value)
+                  }
+                />
+                <TextFieldStyled
+                  name="signupRePassword"
+                  type="password"
+                  placeholder="Retype Password"
+                  value={loginData.signupRePassword}
+                  onChange={(e) =>
+                    inputChange("signupRePassword", e.target.value)
+                  }
+                />
+              </Stack>
+              <Stack
+                flexDirection="row"
+                alignItems="center"
+                justifyContent="center"
+                gap="15px"
+                mt="37px"
+              >
+                <FormControlLabel
+                  control={<Checkbox defaultChecked />}
+                  label="I am not a robot!"
+                />
+                {/* <ReCAPTCHA sitekey="Your client site key" /> */}
+              </Stack>
+              <ButtonLogin variant="yellow">Create Account</ButtonLogin>
+              <Stack alignItems="center" gap="10px">
+                <Switcher
+                  name="keepSignedin"
+                  label="Keep me signed in."
+                  defaultChecked
+                />
+                <Switcher
+                  name="sendEmails"
+                  label="Send me emails about updates and news."
+                  defaultChecked
+                />
+              </Stack>
+              <Divider sx={{ borderColor: "#BFBEBB", my: "40px" }} />
+              <Typography
+                maxWidth="300px"
+                fontSize="12px"
+                fontWeight={500}
+                lineHeight="16px"
+                color="#BFBEBB"
+                margin="auto"
+              >
+                By creating an account, you are agreeing to our{" "}
+                <Link href="" underline="hover" color="#026670">
+                  Terms of Service
+                </Link>{" "}
+                and{" "}
+                <Link href="" underline="hover" color="#026670">
+                  Privacy Policy
+                </Link>
+                .
+              </Typography>
+            </Box>
+          </Stack>
+        </Box>
       </Box>
     </Dialog>
   );
